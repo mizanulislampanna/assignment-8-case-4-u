94ae1a86d6aab04755f6aa866996ae957f343b70
Mizanul Islam Panna <mizanulislampanna>
2022-03-26 13:38:59 +0600

    product dtails added

--- src/Comonents/Products/Product.js
+++ src/Comonents/Products/Product.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-  const { img, name } = props.product;
+  const { img, name, price } = props.product;
   console.log(props);
   return (
     <div className="product">
       <img src={img} alt="" />
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price : ${price}</p>
+      </div>
+      <button className="button-cart">
+        <p>Add To Cart</p>
+      </button>
     </div>
   );
 };
